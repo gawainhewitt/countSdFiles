e0f0812c0e0e6d70ba352ae3e212c499750a6510
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-17 15:21:37 +0100

    working sd file counter

--- countSdFiles.ino
+++ countSdFiles.ino
@@ -1,39 +1,13 @@
-/*
-  SD card basic directory list example
- 
- This example shows how to create and destroy an SD card file 	
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11, pin 7 on Teensy with audio board
- ** MISO - pin 12
- ** CLK - pin 13, pin 14 on Teensy with audio board
- ** CS - pin 4, pin 10 on Teensy with audio board
- 
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 	 
- */
 #include <SD.h>
 
-// change this to match your SD shield or module;
-// Teensy 2.0: pin 0
-// Teensy++ 2.0: pin 20
-// Wiz820+SD board: pin 4
-// Teensy audio board: pin 10
-// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
 const int chipSelect = 10;
 
 void setup()
 {
-  //Uncomment these lines for Teensy 3.x Audio Shield (Rev C)
-  SPI.setMOSI(11);  // Audio shield has MOSI on pin 7
-  SPI.setSCK(13);  // Audio shield has SCK on pin 14  
+
+  SPI.setMOSI(11);  
+  SPI.setSCK(13);  
   
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
     ; // wait for serial port to connect.
@@ -49,7 +23,7 @@ void setup()
 
   File root = SD.open("/");
   
-  printDirectory(root, 0);
+  countDirectory(root);
   
   Serial.println("done!");
 }
@@ -59,56 +33,22 @@ void loop()
   // nothing happens after setup finishes.
 }
 
-void printDirectory(File dir, int numSpaces) {
-   while(true) {
-     File entry = dir.openNextFile();
-     if (! entry) {
-       //Serial.println("** no more files **");
-       break;
-     }
-     printSpaces(numSpaces);
-     Serial.print(entry.name());
-     if (entry.isDirectory()) {
-       Serial.println("/");
-       printDirectory(entry, numSpaces+2);
-     } else {
-       // files have sizes, directories do not
-       unsigned int n = log10(entry.size());
-       if (n > 10) n = 10;
-       printSpaces(50 - numSpaces - strlen(entry.name()) - n);
-       Serial.print("  ");
-       Serial.print(entry.size(), DEC);
-       DateTimeFields datetime;
-       if (entry.getModifyTime(datetime)) {
-         printSpaces(4);
-         printTime(datetime);
-       }
-       Serial.println();
-     }
-     entry.close();
-   }
+void countDirectory(File dir) {
+  int count_files = 0;
+    while(true) {
+      File entry = dir.openNextFile();
+      if (! entry) {
+        Serial.print("number of files = ");
+        Serial.println(count_files -1);
+        return count_files -1;
+        break;
+      }
+      String file_name = entry.name();	//Get file name so that we can check 
+                //if it's a duplicate
+      if( file_name.indexOf('~') != 0 ) 	//Igrnore filenames with a ~. It's a mac thing.
+      {					//Just don't have file names that have a ~ in them
+        count_files++;
+      }
+    }
 }
 
-void printSpaces(int num) {
-  for (int i=0; i < num; i++) {
-    Serial.print(" ");
-  }
-}
-
-void printTime(const DateTimeFields tm) {
-  const char *months[12] = {
-    "January","February","March","April","May","June",
-    "July","August","September","October","November","December"
-  };
-  if (tm.hour < 10) Serial.print('0');
-  Serial.print(tm.hour);
-  Serial.print(':');
-  if (tm.min < 10) Serial.print('0');
-  Serial.print(tm.min);
-  Serial.print("  ");
-  Serial.print(tm.mon < 12 ? months[tm.mon] : "???");
-  Serial.print(" ");
-  Serial.print(tm.mday);
-  Serial.print(", ");
-  Serial.print(tm.year + 1900);
-}
